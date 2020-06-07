e641acb104cf2451777cee7d819b9abd1fcf065e
caglarorhan <caglaror@gmail.com>
2020-06-06 23:04:41 -0500

    Order queue counts and review star counts listing and downloading as SCV

--- js/content.js
+++ js/content.js
@@ -42,4 +42,3 @@ function fetchCategories(){
 }
 
 
-
--- js/popup.js
+++ js/popup.js
@@ -7,9 +7,10 @@ state['tabData']='';
 state['viewList'] = {};
 state['failedURLz']=[];
 state['crawlStatusOrder']={continue:true};
-state['crawlStatusChangerButton'] = {position:true}
+state['crawlStatusChangerButton'] = {position:true};
 state['freshCategories']={refreshDate:minutesNow, categoryList:[]};
 state['firebaseConfig'] = {};
+state['settings']={console:true, toast:true};
 state['countries']= ["Argentina", "Australia", "Bahrain", "Bangladesh", "Barbados", "Bosnia and Herzegovina", "Bulgaria", "Cameroon", "Canada", "China", "Colombia", "Croatia", "Cyprus", "Czech Republic", "Dominican Republic", "Ecuador", "Egypt", "France", "Germany", "Ghana", "Greece", "Hong Kong", "Hungary", "India", "Indonesia", "Ireland", "Israel", "Italy", "Kenya", "Lithuania", "Macedonia [FYROM]", "Malaysia", "Moldova", "Morocco", "Nepal", "Netherlands", "New Zealand", "Nigeria", "Norway", "Oman", "Pakistan", "Peru", "Philippines", "Poland", "Portugal", "Romania", "Russia", "Saudi Arabia", "Serbia", "Slovenia", "Spain", "Sri Lanka", "Suriname", "Sweden", "Switzerland", "Thailand", "Turkey", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam", "Zambia"];
 state['languages'] =["Arabic", "Bengali", "Chinese", "Dutch", "English", "French", "German", "Gujarati", "Hebrew", "Hindi", "Italian", "Kannada", "Malayalam", "Marathi", "Oriya", "Persian", "Polish", "Portuguese", "Portuguese-BR", "Punjabi", "Russian", "Spanish", "Tamil", "Telugu", "Thai", "Turkish", "Ukrainian", "Urdu", "Vietnamese"];
 
@@ -73,6 +74,16 @@ function saveGigToFireStore(gG){
         });
 }
 
+function pushToDownloadTextFile(ingredient){
+    let blob = new Blob([ingredient], {type: "text/plain"});
+    let url = URL.createObjectURL(blob);
+    chrome.downloads.download({
+        url: url // The object URL can be used as download URL
+        //...
+    });
+}
+
+
 
 function totalLoad(){
     let settingsTab = M.Tabs.init(document.querySelector('#settingTabs'));
@@ -124,16 +135,59 @@ function totalLoad(){
         M.toast({html:'Firebase configuration text saved into localStorage!'})
     });
 
-    //reportsButton
-    document.querySelector('#reportsButton').addEventListener('click',()=>{
-        document.querySelector('#reports').innerHTML = gimmeReport();
-    })
-
     // getURL list
     document.querySelector('#getTheCategoryURLzButton').addEventListener('click',async ()=>{
         document.querySelector('#urlList').innerHTML= loadingCircle();
         let fC = await getCategories();
-        let divCreation = await urlDivCreator(fC);
+        let divCreation = await urlDivCreator(fC,'urlList');
+    });
+
+    //setting console status button
+    document.querySelector('#settingsConsoleStatus').addEventListener('click',()=>{
+        m2c({value:state});
+        state.settings.console=this.checked;
+    });
+
+    //setting toast messages
+    document.querySelector('#settingsToastStatus').addEventListener('click',()=>{
+        state.settings.toast=this.checked;
+        if(state.settings.toast){
+            let addStyle = document.createElement('style');
+            //addStyle.id='toastContainerDisplaySetting';
+            addStyle.innerHTML = `
+            #toast-container{ display:none !important;}
+            `;
+            document.head.append(addStyle);
+        }else{
+            document.querySelector('#toastContainerDisplaySetting').remove();
+        }
+
+
+    });
+
+    //reports
+    document.querySelector('#reportsButton').addEventListener('click',async()=>{
+        document.querySelector('#reports').innerHTML= loadingCircle();
+        let fC = await getCategories();
+        let divCreation = await urlDivCreator(fC,'reports');
+        locSt.setItem('orderQueueData','');
+        let orderQueTotals = await urlRequester(fC);
+        m2c({value:orderQueTotals});
+        orderQueTotals='Category Total Queue Count, 5 Star Count, 4 Star Count, 3 Star Count, 2 Star Count, 1 Star Count' +'\n' +orderQueTotals;
+        locSt.setItem('orderQueueData',orderQueTotals);
+
+        pushToDownloadTextFile(orderQueTotals);
+    });
+
+    //floating button init
+    let instances = M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'), {
+        direction: 'left',
+        hoverEnabled: false
+    });
+
+    // reports floating button click
+    document.querySelector('#reportsDownloadCsvFileButton').addEventListener('click',()=>{
+        pushToDownloadTextFile(locSt.getItem('orderQueueData'));
     })
 
 
@@ -141,12 +195,103 @@ function totalLoad(){
 }
 
 
-function gimmeReport(){
-    m2c({value:'Not tested because of quota problems.'});
-    // fF.db.collection('gigs').get().then(function(snapshot) {
-    //     //m2c({value:snapshot.doc.seller_name});
-    // });
+async function urlRequester(fC){
+    let totalUrlValues='';
+    let divHeight = 25;
+    for(let orderNum=1; orderNum<2; orderNum++){ //her temel url buradan giriyor
+        document.querySelector('#processingURLdiv_'+orderNum).classList.remove('cumulDiv');
+        document.querySelector('#processingURLdiv_'+orderNum).classList.add('cumulDivActive');
+        document.querySelector('#processingURLPageNumber_'+orderNum).textContent=`process`;
+        document.querySelector('#reports').scrollTo({top: divHeight*orderNum , behavior: 'smooth'});
+        let urlData = await sumQueue('https://www.fiverr.com/categories/'+fC[orderNum]+'?ref=seller_level%3Atop_rated_seller');
+        m2c({value:fC[orderNum]});
+        m2c({value:urlData});
+        totalUrlValues+= fC[orderNum]+','+urlData.pagesTotalOrderQueue +','+urlData.starsTotal.toString()+'\n';
+        // toplamlari satirlara yazdir burada
+        //{pagesTotalOrderQueue: pagesTotalOrderQueue, starsTotal: starsTotal }
+        document.querySelector('#processingURLPageNumber_'+orderNum).textContent = urlData.pagesTotalOrderQueue;
+        document.querySelector('#processingURLPageNumber_'+orderNum).title = urlData.starsTotal;
+    }
+return new Promise(resolve=>{
+    resolve(totalUrlValues)
+})
 }
+
+
+
+async function sumQueue(targetHTMLurl){
+    let pagesTotalOrderQueue = 0;
+    let starsTotal = [0,0,0,0,0];
+    //  url yi fetch et page=1 olarak request yap
+    m2c({value:'HEDEF: '+targetHTMLurl});
+    for(let page=1; page<200; page++){
+        let response = await fetch(targetHTMLurl+'&page='+page); // 48 gig ve altinda paging olan sayfa gelecek
+        if (response.status !== 200) {break;}
+        let tempHTML = await response.text();
+        //m2c({value:tempHTML})
+        let tmp = JSON.parse(tempHTML.split('initialData.search_perseus = ')[1].split(';\n' +
+            '  </script>')[0]);
+        if(!tmp.tracking.error){
+            // sayfa son sayfa degil devam
+            let gigSize = tmp.listings[0].gigs.length;//48 donecek
+            //m2c({value:gigSize})
+            let gigsTotalOrderQueue=0;
+            for(let order = 0; order<gigSize; order++){
+                let thisGigTotalOrderQueue = 0;
+                // gigs icindeki her nesne icin gig_url urlsine gidilecek
+                // "https://www.fiverr.com" + tmp.listings[0].gigs[order]
+                //m2c({value:"https://www.fiverr.com" + tmp.listings[0].gigs[order].gig_url});
+
+                let gigPage = await (()=>{
+                    return new Promise((resolve,reject)=>{
+                        window.setTimeout(()=>{
+                            resolve(fetch("https://www.fiverr.com" + tmp.listings[0].gigs[order].gig_url))
+                        },1000)
+                    })
+                })();
+
+                if(gigPage.status!==200){break;}
+                let pageSrc= await gigPage.text();
+                //m2c({value:pageSrc})
+                //m2c({value:tmp.listings[0].gigs[order].seller_name})
+                document.querySelector('#currentSellerName').textContent=tmp.listings[0].gigs[order].seller_name;
+                let domparser = new DOMParser();
+                let doc = domparser.parseFromString(pageSrc, "text/html");
+                //m2c({value:parseInt(doc.querySelector('.orders-in-queue').textContent)})
+                //orders in queue
+                if(doc.querySelector('.orders-in-queue')){
+                    thisGigTotalOrderQueue = parseInt(doc.querySelector('.orders-in-queue').textContent);
+                }
+                //stars
+                if(doc.querySelector(`table.stars-counters`)){
+                    for(let star=1; star<6; star++){
+                        let starPointText = doc.querySelector(`table.stars-counters tr:nth-of-type(${star}) td.star-num`).textContent;
+                        //m2c({value:starPointText});
+                        let starPoint = parseInt(starPointText.replace(/"/g, "").replace(")","").replace("(","").replace(",",""));
+                        //m2c({value:starPoint});
+                        starsTotal[star-1]+=starPoint;
+                    }
+                }
+
+                gigsTotalOrderQueue+=thisGigTotalOrderQueue;
+            }
+            pagesTotalOrderQueue+=gigsTotalOrderQueue;
+
+        }else{
+            break
+        }
+    }
+    //
+    return new Promise(resolve=>{
+        window.setTimeout(()=>{
+            resolve({pagesTotalOrderQueue: pagesTotalOrderQueue, starsTotal: starsTotal })
+        },1000)
+    })
+}
+
+
+
+
 
 
 
@@ -198,6 +343,7 @@ function hideAllContainers(exceptThat){
 
 //Sending message to content side
 function m2c(messageToContentSide){
+    if(!state.settings.console){return false;}
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, messageToContentSide, function (response) {
 
@@ -246,8 +392,8 @@ function loadingCircle(){
   </div>`
 }
 
-function urlDivCreator(urlList){
-    document.querySelector('#urlList').innerHTML= loadingCircle();
+function urlDivCreator(urlList,targetDivId){
+    document.querySelector('#'+targetDivId).innerHTML= loadingCircle();
     let orderNumber=0;
     let cumulDiv=``;
     for(let oUrl of urlList){
@@ -268,7 +414,7 @@ function urlDivCreator(urlList){
                     </div>`;
         orderNumber++;
     }
-    document.querySelector('#urlList').innerHTML=cumulDiv;
+    document.querySelector('#'+targetDivId).innerHTML=cumulDiv;
     return new Promise((res)=>{res(true)});
 }
 
@@ -277,7 +423,7 @@ const crawlIt = async function() {
     if(!checkFirebaseConnection()){return false;}
     state.crawlStatusOrder.continue=true;
     let fC = await getCategories();
-    let divCreation = await urlDivCreator(fC); // urllerin divleri olusturuldu
+    let divCreation = await urlDivCreator(fC,'urlList');
     let orderNum = document.querySelector('#urlOrderNum').value;
     let divHeight = 25;
 
@@ -299,14 +445,14 @@ const crawlIt = async function() {
 // Hedef json sayfasindan seller verilerini ceken kisim
 
 function getJSON(targetJSONurl,orderNum){
-
+//https://www.fiverr.com/categories/choose-website-platform/quiz  .json olmadigindan baska sayfaya yonlendirme var
     return new Promise(async (resolve)=>{
         //--------------------------------------------
         for(let page=1; page<500;page++){
             let donen = await returnMyJson(targetJSONurl, page, orderNum);
-            //m2c({value:`Donen durum:${donen}`});
+            m2c({value:`Donen durum:${donen}`});
             if(!state.crawlStatusOrder.continue){break;}
-               if(!donen){break}
+               if(!donen){break;}
         }
 
 
@@ -319,11 +465,12 @@ function getJSON(targetJSONurl,orderNum){
 }
 
 async function returnMyJson(targetJSONurl,page, orderNum){
+    ////https://www.fiverr.com/categories/choose-website-platform/quiz  .json olmadigindan baska sayfaya yonlendirme var  duz html sayfa geliyor... json yok
     let response = await fetch(targetJSONurl+'.json?page='+page);
+    //m2c({value:response.status})
+        if (response.status !== 200) { state['failedURLz'].push(targetJSONurl); return new Promise(res=>{res(false)});}
     let jsonData = await response.json();
     //m2c({value:jsonData});
-
-        if (response.status !== 200) { state['failedURLz'].push(targetJSONurl); return new Promise(res=>{res(false)});}
     document.querySelector('#processingURLdiv_'+orderNum).classList.add('cumulDivActive');
     document.querySelector('#processingURLdiv_'+orderNum).classList.remove('cumulDiv');
     for(let title of jsonData.gigs){
